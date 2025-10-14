8b335cc8ebf4d7f90a1077a4f115b35c2a176a11
Gabriel Felipe <gabrielfsoliveiradev@gmail.com>
2025-10-14 19:05:30 -0300

    Conectando ao servidor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import connectOnDB from "./config/dbConnect.js";
 
 const app = express()
+
+const connection = await connectOnDB()
+connection.on("error", (error) => console.error(`${error} - FALHA AO TENTAR CONECTAR AO SERVIDOR`))
+connection.once("open", () => console.log("CONEXÃO COM SERVIDOR ESTABELECIDA"))
 
 export default app
