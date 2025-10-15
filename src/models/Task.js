215b661f7e9f72cdd6f65ee034ec7e9fcdcc9732
Gabriel Felipe <gabrielfsoliveiradev@gmail.com>
2025-10-14 21:34:21 -0300

    Data de tasks agora é um campo required: true

--- src/models/Task.js
+++ src/models/Task.js
@@ -5,7 +5,7 @@ const taskSchema = new Schema({
   title: {type: String, required: true} ,
   descripiton: {type: String},
   status: {type: String, default: "todo", enum: ["todo", "in_progress", "done"]},
-  created_at: { type: Date },
+  created_at: { type: Schema.Types.Date, required: true },
   updated_at: { type: Date }
 }, {versionKey: false})
 
